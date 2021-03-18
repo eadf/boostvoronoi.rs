4470400fd543ce9a852e3b955394a1fc4651a0d5
eadf <ead.fritz@gmail.com>
2021-03-18 16:56:56 +0100

    Added the README example

--- src/builder.rs
+++ src/builder.rs
@@ -47,7 +47,31 @@ mod tests;
 /// container was chosen to hold state of the beach line. The keys of the map
 /// correspond to the neighboring sites that form a bisector and values map to
 /// the corresponding Voronoi edges in the output data structure.
-
+/// ```
+/// # use boostvoronoi::{Point,Line};
+/// # use boostvoronoi::builder::{Builder};
+///
+/// type I1 = i32; // this is the integer input type
+/// type F1 = f64; // this is the float output type (circle event coordinates)
+/// type I2 = i64; // All integer calculations are made in this type (or num::BigInt when needed)
+/// type F2 = f64; // All float calculations are made in this type
+/// // it is ok to set I1=I2=i64 and F1=F2=f64
+///
+/// // Points should be unique,
+/// let p = vec![Point{x:9_i32, y:10}];
+/// // Lines should never intersect with other lines.
+/// // The only points that can intersect are the endpoints.
+/// let s = vec![Line::new(Point{x:10_i32, y:11}, Point{x:12, y:13})];
+/// let mut vb = Builder::<I1, F1, I2, F2>::new();
+///
+/// // you will have to keep track of the input geometry. it will be referenced as
+/// // input geometry indices in the output.
+/// vb.with_vertices(p.iter()).unwrap();
+/// vb.with_segments(s.iter()).unwrap();
+///
+/// // this will generate a the list of cells, edges and circle events (aka vertices)
+/// let result = vb.construct().unwrap();
+/// ```
 pub struct Builder<I1, F1, I2, F2>
 where
     I1: InputType + Neg<Output = I1>,
